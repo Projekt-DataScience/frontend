45d0eaa2ba4842b3ed3797944e3f71ec8333484f
Jonas <jonas.mildner@stud.hs-kempten.de>
2022-11-10 12:16:12 +0100

    Initial Commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
+    require("prettier-plugin-tailwindcss")
+  ],
+};
