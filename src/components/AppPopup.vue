6f99eeab23a4d8044b25fa1de571755e32e7ff06
raphaelseliger <raphael.seliger@web.de>
2022-11-30 21:22:06 +0100

    Update Audit page

---
+++ src/components/AppPopup.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>Test</div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AppListText",
+  props: {
+    text: {
+        type: String,
+        required: true
+    }
+  }
+});
+</script>
