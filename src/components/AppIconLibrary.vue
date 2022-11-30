bc7909b2cb443d5981cd9bef88f3affe1c7846c7
raphaelseliger <raphael.seliger@web.de>
2022-11-30 10:42:31 +0100

    Update Icon Library

---
+++ src/components/AppIconLibrary.vue
@@ -0,0 +1,33 @@
+<template>
+    
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AppListText",
+  props: {
+    name: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: false
+    },
+    width: {
+
+    },
+    height: {
+
+    },
+    stroke: {
+
+    },
+    color: {
+
+    }
+  }
+});
+</script>
