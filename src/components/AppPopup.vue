f5e959b99fa86698119b5268a681cb92612b15fa
Raphael Seliger <raphael.seliger@web.de>
2022-12-01 15:50:19 +0100

    Update Popup component

--- src/components/AppPopup.vue
+++ src/components/AppPopup.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="fixed top-0 bottom-0 left-0 right-0 flex justify-center items-center z-30 bg-black/50">
-      <div class="bg-white rounded-xl max-w-screen-md">
+      <div class="bg-white rounded-xl max-w-screen-md min-w-1/2">
         <slot></slot>
       </div>
     </div>
