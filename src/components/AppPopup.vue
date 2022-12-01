be59691e2f1db5286fc3309650722a4b0bb808e4
Raphael Seliger <raphael.seliger@web.de>
2022-12-01 08:37:29 +0100

    Update Popup

--- src/components/AppPopup.vue
+++ src/components/AppPopup.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="fixed top-0 bottom-0 left-0 right-0 flex justify-center items-center z-30 bg-cl-popup">
+    <div class="fixed top-0 bottom-0 left-0 right-0 flex justify-center items-center z-30 bg-black/50">
       <div class="bg-white p-10 rounded-md"></div>
     </div>
 </template>
