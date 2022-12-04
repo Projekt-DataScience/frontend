7afb28d7990817f4f501b33428166ca7b6912b43
Jonas <jonas.mildner@stud.hs-kempten.de>
2022-12-04 16:29:17 +0100

    Initial Login Page

---
+++ src/views/Login.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="grid grid-cols-4">
+        <div class="col-span-1">
+            <AppButtonPrimary name="Einloggen" is-active="true">
+            </AppButtonPrimary>
+        </div>
+        <div class="col-span-auto">
+            TEST
+        </div>
+    </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import AppButtonPrimary from "../components/AppButtonPrimary.vue";
+
+export default defineComponent({
+    name: "Login",
+    components: {
+        AppButtonPrimary
+    },
+    setup() {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    methods: {
+
+    }
+}); 
+</script>
