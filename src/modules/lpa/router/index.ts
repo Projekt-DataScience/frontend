c9e2d2bd5c14e98d870401e3e3aa3a1de814135c
Raphael Seliger <raphael.seliger@web.de>
2022-12-20 14:27:14 +0100

    Update LPA Router

--- src/modules/lpa/router/index.ts
+++ src/modules/lpa/router/index.ts
@@ -17,6 +17,9 @@ export const lpaRoutes = [
                 path: '',
                 name: 'LPADashboard',
                 component: LPADashboard,
+                meta: {
+                    requiresAuth: true
+                }
             },
             {
                 // UserProfile will be rendered inside User's <router-view>
@@ -24,31 +27,41 @@ export const lpaRoutes = [
                 path: 'fragen',
                 name: 'LPAQuestions',
                 component: LPAQuestions,
+                meta: {
+                    requiresAuth: true
+                }
             },
             {
                 path: 'audit/',
                 name: 'LPAAudit',
                 component: LPAAudit,
+                meta: {
+                    requiresAuth: true
+                }
             },
             {
                 path: 'analysen',
                 name: 'LPAAnalytics',
                 component: LPAAnalytics,
+                meta: {
+                    requiresAuth: true
+                }
             },
             {
                 path: 'konfiguration',
                 name: 'LPAConfiguration',
                 component: LPAConfiguration,
+                meta: {
+                    requiresAuth: true
+                }
             },
             {
                 path: 'historie',
                 name: 'LPAHistory',
                 component: LPAHistory,
-            },
-            {
-                path: 'test',
-                name: 'Test',
-                component: Test,
+                meta: {
+                    requiresAuth: true
+                }
             }
         ]
     }
