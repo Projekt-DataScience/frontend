27aafd3780459674f4bffa442b3d98167912dff0
raphaelseliger <50147613+raphaelseliger@users.noreply.github.com>
2022-12-28 09:04:33 +0100

    Update Audit Score

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import './style.css'
 import App from './App.vue'
 import router from './router/index'
 import { createPinia } from "pinia";
+import VueApexCharts from "vue3-apexcharts";
 
 /* Register Modules
 import { registerModules } from "./register-modules";
@@ -14,5 +15,6 @@ registerModules({
 
 const app = createApp(App);
 app.use(createPinia())
+app.use(VueApexCharts)
 app.use(router)
 app.mount('#app');
